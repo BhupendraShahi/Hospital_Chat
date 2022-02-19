6e0ca9e927044841feab0c8d77d90d6abc6663c0
Bhupendra <shahi23.bhupendra11@gmail.com>
2022-02-19 10:43:57 +0530

    work on add channel

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Channel} from 'stream-chat-react';
+import { Channel, useChatContext, MessageTeam} from 'stream-chat-react';
 
-import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
+import { ChannelInner, CreateChannel, EditChannel} from './';
 
 const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
     if (isCreating) {
@@ -23,7 +23,7 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
     const EmptyState = () => {
         return (
             <div className='Channel-empty__container'>
-                <p className='channel-empty__first'>this is the begining of your chat history</p>
+                <p className='channel-empty__first'>this is the beginning of your chat history</p>
                 <p className='channel-empty__second'>send Messages, attachments, links, emojis, and more!</p>
             </div>
         )
@@ -33,7 +33,7 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
         <div className='channel__container'>
             <Channel
                 EmptyStateIndicator={EmptyState}
-                Message={(messageProps, i) => <TeamMessage key={i} {...messageProps} />}
+                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
             >
                 <ChannelInner setIsEditing={setIsEditing} />
             </Channel>
--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { AddChannel  } from '../assets';
 
-const TeamChannelList = ({ children, error = false, loading, type }) => {
+const TeamChannelList = ({ children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
   if (error) {
     return type === 'team' ? (
       <div className='team-channel-list'>
@@ -26,10 +26,16 @@ const TeamChannelList = ({ children, error = false, loading, type }) => {
   return (
     <div className='team-channel-list'>
       <div className='team-channel-list__header'>
-        <p className='team-channel__header__title'>
+        <p className='team-channel-list__header__title'>
           {type==='team' ? 'channels' : 'Direct Messages'}
         </p>
-        {/* Button - add Channel */}
+        <AddChannel
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+          type={type === 'team' ? 'team' : 'messaging'}
+        />
       </div>
       {children}
     </div>
